746bf9ed30d15d9be939c0f41c0abfdc306c6272
oncletom <thomas@oncle-tom.net>
2012-05-22 10:57:36 +0200

    Reorganized the build script

--- bin/build.js
+++ bin/build.js
@@ -3,23 +3,55 @@
 "use strict";
 
 var request = require('request');
-var parser = require('../lib/rule-parser.js').init();
-require('shelljs/global');
+var ruleParser = require('../lib/rule-parser.js');
+require('shelljs/make');
 
-//@todo put that in a default optimist value
-var dataUri = 'http://mxr.mozilla.org/mozilla-central/source/netwerk/dns/effective_tld_names.dat?raw=1';
-
-/*
- * Builds a new static file containing public suffix rules
- * The kind of feature missing in other TLDs projects
+/**
+ * Builds the whole stuff. Sorry, you won't have time to take a coffee
  */
-request(dataUri, function (err, response, body) {
-  if (err) {
-    throw Error(err);
-  }
+target.build = function(){
+  target.download();
+  target.buildRules();
+  target.package();
+};
 
-  var rules = JSON.stringify(parser.parse(body));
+/**
+ * Download a fresh copy of data from the cloud (aka Internet in the ancient times)
+ */
+target.download = function(){
+  //@todo move that in a cli/argument
+  var dataUri = 'http://mxr.mozilla.org/mozilla-central/source/netwerk/dns/effective_tld_names.dat?raw=1';
 
-  rules.to('src/rules.json');
-  parser.wrap(rules).to('src/tld.rules.js');
-});
+  request(dataUri, function (err, response, body) {
+    if (err) {
+      throw Error(err);
+    }
+
+    body.to('data/effective_tld_names.dat');
+  });
+};
+
+/**
+ * Build the rules from a dataset from publicsuffix.org
+ */
+target.buildRules = function(){
+  var parser, rules;
+
+  parser = (new ruleParser()).parse(cat('data/effective_tld_names.dat'));
+  parser.toString().to('src/rules.json');
+};
+
+/**
+ * Packages the rules to an optimized frontend build
+ */
+target.package = function(){
+  var rules = require('src/rules.json');
+
+  wrapRules(rules).to('src/tld.rules.js');
+};
+
+function wrapRules(rules){
+  return 'tld.prototype.augment = function(){' +
+    'this.rules = '+ JSON.stringify(rules) +';' +
+  '};';
+}
